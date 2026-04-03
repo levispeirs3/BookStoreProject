177c1df54847babf1034827a00403a28ccb27f06
levispeirs3 <speirs03@byu.edu>
2026-04-02 18:43:46 -0600

    Made changes to CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://happy-pebble-0a167f41e.1.azurestaticapps.net")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
